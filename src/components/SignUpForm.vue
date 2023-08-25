6c1965e32b0fbaca72b72310c07d5406b35c2885
PaulDreyer09 <52189254+PaulDreyer09@users.noreply.github.com>
2023-08-25 21:45:13 +0200

    Implemented Email, Password, Role, Skills, Terms fields

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,10 +1,34 @@
 <template>
-  <form>
-    <label>Email: </label>
-    <input type="text" required v-model="email">
-    <label>Password: </label>
-    <input type="text" required v-model="password">
-  </form>
+    <form>
+        <label>Email: </label>
+        <input type="text" required v-model="email">
+
+        <label>Password: </label>
+        <input type="password" required v-model="password">
+
+        <label>Role: </label>
+        <select v-model="role">
+            <option value="developer">Web Developer</option>
+            <option value="designer">Web Designer</option>
+        </select>
+
+        <label >Skills: </label>
+        <input type="text" v-model="tempSkill" @keydown="addSkill" @keypress="preventCommaInput" >
+        <div v-for="skill in skills" :key="skill" class="pill" @click="removeSkill(skill)">
+            {{ skill }}
+        </div>
+
+        <div class="terms">
+            <input type="checkbox" required v-model="terms">
+            <label>Accept terms and conditions</label>
+        </div>
+    </form>
+
+    <p>Email: {{ email }}</p>
+    <p>Password: {{ password }}</p>
+    <p>Role: {{ role }}</p>
+    <p>Terms accepter: {{ terms }}</p>
+
 </template>
 
 <script>
@@ -12,7 +36,30 @@ export default {
     data() {
         return {
             email: '',
+            password: '',
+            role: '',
+            terms: false,
+            tempSkill: '',
+            skills: [],
 
+        }
+    },
+    methods: {
+        addSkill(e){
+            if(e.key === ',' && this.tempSkill ){
+                if (!this.skills.includes(this.tempSkill)){                    
+                    this.skills.push(this.tempSkill);
+                }
+                this.tempSkill = '';
+            }
+        },
+        preventCommaInput(e){
+            if(e.key === ','){
+                e.preventDefault();
+            }
+        },
+        removeSkill(skill){
+            this.skills = this.skills.filter(item => item != skill);
         }
     }
 }
@@ -39,7 +86,7 @@ export default {
         font-weight: 600;
     }
 
-    input{
+    input, select{
         display: block;
         padding: 10px 6px;
         width: 100%;
@@ -52,5 +99,27 @@ export default {
     input:focus{
         border: 1px solid #ddd !important;
     }
+
+    input[type="checkbox"]{
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
+    }
+
+    .pill{
+        display: inline-block;
+        margin: 20px 10px 0 0;
+        padding: 6px 12px;
+        background: #eee;
+        border-radius: 20px;
+        font-size: 12px;
+        letter-spacing: 1px;
+        font-weight: bold;
+        color: #777;
+        cursor: pointer;
+    }
+
 
 </style>
